c0d7500a1f0c6a9dfc9be17050f7f29568299829
Williamz21 <u201816224@upc.edu.pe>
2022-04-22 14:01:02 -0500

    feat: controller, service repository of teachers added

---
+++ Asimov.API/Teachers/Domain/Models/TeacherCourse.cs
@@ -0,0 +1,13 @@
+﻿using Asimov.API.Courses.Domain.Models;
+
+namespace Asimov.API.Teachers.Domain.Models
+{
+    public class TeacherCourse
+    {
+        public int TeacherId { get; set; }
+        public int CourseId { get; set; }
+        
+        public Teacher Teacher { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Asimov.API/Teachers/Domain/Services/Communication/TeacherResponse.cs
@@ -0,0 +1,16 @@
+﻿using Asimov.API.Shared.Domain.Services.Communication;
+using Asimov.API.Teachers.Domain.Models;
+
+namespace Asimov.API.Teachers.Domain.Services.Communication
+{
+    public class TeacherResponse : BaseResponse<Teacher>
+    {
+        public TeacherResponse(string message) : base(message)
+        {
+        }
+
+        public TeacherResponse(Teacher resource) : base(resource)
+        {
+        }
+    }
+}
