ee7739338e55dc5925a03a274e22ca4fe39e0d0d
JulioSalazar12 <u202017572@upc.edu.pe>
2022-04-22 14:33:47 -0500

    feat: element shared added

---
+++ Asimov.API/Shared/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Asimov.API.Shared.Domain.Repositories;
+using Asimov.API.Shared.Persistence.Contexts;
+
+namespace Asimov.API.Shared.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
