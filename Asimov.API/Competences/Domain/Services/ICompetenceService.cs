a2c876d9dc00f311eaaee6853fa78a68f4cda9ac
S-011 <u201811286@upc.edu.pe>
2022-04-22 14:35:25 -0500

    feat: controller, service and repository of Competences added

---
+++ Asimov.API/Competences/Domain/Services/ICompetenceService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Asimov.API.Competences.Domain.Models;
+using Asimov.API.Competences.Domain.Services.Communication;
+
+namespace Asimov.API.Competences.Domain.Services
+{
+    public interface ICompetenceService
+    {
+        Task<IEnumerable<Competence>> ListAsync();
+        Task<CompetenceResponse> SaveAsync(Competence competence);
+        Task<CompetenceResponse> UpdateAsync(int id, Competence competence);
+        Task<CompetenceResponse> DeleteAsync(int id);
+    }
+}
---
+++ Asimov.API/Competences/Persistence/Repositories/CompetenceRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Asimov.API.Competences.Domain.Models;
+using Asimov.API.Competences.Domain.Repositories;
+using Asimov.API.Shared.Persistence.Contexts;
+using Asimov.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asimov.API.Competences.Persistence.Repositories
+{
+    public class CompetenceRepository : BaseRepository, ICompetenceRepository
+    {
+        public CompetenceRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Competence>> ListAsync()
+        {
+            return await _context.Competences.ToListAsync();
+        }
+
+        public async Task AddAsync(Competence competence)
+        {
+            await _context.Competences.AddAsync(competence);
+        }
+
+        public async Task<Competence> FindByIdAsync(int id)
+        {
+            return await _context.Competences.FindAsync(id);
+        }
+
+        public void Update(Competence competence)
+        {
+            _context.Competences.Update(competence);
+        }
+
+        public void Remove(Competence competence)
+        {
+            _context.Competences.Remove(competence);
+        }
+    }
+}
