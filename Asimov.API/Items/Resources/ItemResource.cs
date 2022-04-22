85fcef767005604446ac531c33c569a8db2aeb08
Williamz21 <u201816224@upc.edu.pe>
2022-04-22 14:30:49 -0500

    feat: controller, service and repository of item added

---
+++ Asimov.API/Items/Resources/ItemResource.cs
@@ -0,0 +1,12 @@
+﻿namespace Asimov.API.Items.Resources
+{
+    public class ItemResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool State { get; set; }
+        
+        public int CourseId { get; set; }
+    }
+}
