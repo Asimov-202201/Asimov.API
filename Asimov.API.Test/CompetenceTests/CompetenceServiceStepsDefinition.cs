6212c4ee52a588336c20261dee068c5b31054289
Yordy-code81 <U201923959@upc.edu.pe>
2022-05-05 15:03:35 -0500

    feat: competence test added

---
+++ Asimov.API.Test/CompetenceTests/CompetenceServiceStepsDefinition.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Asimov.API.Competences.Resources;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using SpecFlow.Internal.Json;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace Asimov.API.Test.CompetenceTests
+{
+    [Binding]
+    public class CompetenceServiceStepsDefinition
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        private HttpClient Client { get; set; }
+        private Uri BaseUri { get; set; }
+        private Task<HttpResponseMessage> Response { get; set; }
+
+        public CompetenceServiceStepsDefinition(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/competences is available")]
+        public void GivenTheEndpointHttpsLocalhostApiVCompetencesIsAvailable(int port, int version)
+        {
+            BaseUri = new Uri($"https://localhost:{port}/api/v{version}/competences");
+            Client = _factory.CreateClient(new WebApplicationFactoryClientOptions {BaseAddress = BaseUri});
+        }
+
+        
+        [When(@"A Post Request to Competence is sent")]
+        public void WhenAPostRequestToCompetenceIsSent(Table saveCompetenceResource)
+        {
+            var resource = saveCompetenceResource.CreateSet<SaveCompetenceResource>().First();
+            var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+            Response = Client.PostAsync(BaseUri, content);
+        }
+        
+        [Then(@"A Response with Status (.*) is received in Competence")]
+        public void ThenAResponseWithStatusIsReceivedInCompetence(int expectedStatus)
+        {
+            var expectedStatusCode = ((HttpStatusCode) expectedStatus).ToString();
+            var actualStatusCode = Response.Result.StatusCode.ToString();
+            Assert.Equal(expectedStatusCode, actualStatusCode);
+        }
+
+        [Then(@"A Competence Resource is included in Response body")]
+        public async void ThenACompetenceResourceIsIncludedInResponseBody(Table expectedCompetenceResource)
+        {
+            var expectedResource = expectedCompetenceResource.CreateSet<CompetenceResource>().First();
+            var responseData = await Response.Result.Content.ReadAsStringAsync();
+            var resource = JsonConvert.DeserializeObject<CompetenceResource>(responseData);
+            expectedResource.Id = resource.Id;
+            var jsonExpectedResource = expectedResource.ToJson();
+            var jsonActualResource = resource.ToJson();
+            Assert.Equal(jsonExpectedResource, jsonActualResource);
+        }
+    }
+}
